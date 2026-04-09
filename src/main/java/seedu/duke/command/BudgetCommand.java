e9cdf85057cdd3f75c9ce7db5f3c2ac3aa1f71b8
AfshalG <afshalkingmaker@gmail.com>
2026-04-10 00:29:52 +0800

    fix: require minimum budget of $0.01 to prevent sub-cent rounding

--- src/main/java/seedu/duke/command/BudgetCommand.java
+++ src/main/java/seedu/duke/command/BudgetCommand.java
@@ -64,6 +64,11 @@ public class BudgetCommand extends Command {
         if (amount <= 0) {
             throw new SpendTrackException("Budget must be greater than $0.00.");
         }
+        // @@author AfshalG
+        if (amount < 0.01) {
+            throw new SpendTrackException("Budget must be at least $0.01.");
+        }
+        // @@author
         if (amount > MAX_BUDGET) {
             throw new SpendTrackException("Budget cannot exceed $1,000,000.00.");
         }
